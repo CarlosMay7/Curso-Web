c0c65dee257e8994ab5b15443c4af0a791fb0963
Carlos May <carlosmay220@gmail.com>
2023-06-03 01:05:29 -0600

    Avances sección 42

--- BienesRaicesMVC/controllers/PaginasController.php
+++ BienesRaicesMVC/controllers/PaginasController.php
@@ -4,6 +4,7 @@
 namespace Controllers;
 use MVC\Router;
 use Model\Propiedad;
+use PHPMailer\PHPMailer\PHPMailer;
 
 class PaginasController {
 
@@ -40,7 +41,42 @@ class PaginasController {
         $router->render("paginas/entrada");
     }
     public static function contacto(Router $router){
-        $router->render("paginas/contacto");
+
+        if($_SERVER["REQUEST_METHOD"] === "POST"){
+            //Crear instancia phpmailer
+            $mail = new PHPMailer();
+            //Configurar SMTP (Protocolo para envio de emails)
+            $mail->isSMTP();
+            $mail->Host ='sandbox.smtp.mailtrap.io';
+            $mail->SMTPAuth = true; //Define si se necesita autenticar
+            $mail->Username = '856107c79736a8';
+            $mail->Password = 'f06aa13c680c16';
+            $mail->SMTPSecure = 'tls'; //Metodo de enio de datos
+            $mail->Port = 2525;
+
+            //Configurar contenido email
+            $mail->setFrom("admin@bienesraices.com");
+            $mail->addAddress("admin@bienesraices.com", "BienesRaices.com"); //Quien lo recibe y donde
+            $mail->Subject = "Tienes un nuevo mensaje";
+
+            //Habilitar HTML
+            $mail->isHTML(true);
+            $mail->CharSet = "UTF-8";
+
+            //Definir el contenido
+            $contenido = "<html> <p>Tienes un nuevo mensaje</p> </html>";
+
+            $mail->Body = $contenido;
+            $mail->AltBody = "Texto sin html"; //Cuando el lector de mails no entiende el HTML
+
+            //Enviar al email
+            if ($mail->send()){
+                echo "Mensaje enviado";
+            } else {
+                echo "Mensaje no enviado";
+            }
+        }
+        $router->render("paginas/contacto", []);
     }
 
 }
